d92d7a03a2a9b2cf7083c706c8d6688885ab21f8
Krauser Huang <crescentdence@gmail.com>
2024-05-19 14:14:55 +0800

    NextViewController update [progress]

--- SwiftfulSourceControlDemo/NextViewController.swift
+++ SwiftfulSourceControlDemo/NextViewController.swift
@@ -11,8 +11,8 @@ class NextViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
     
 
