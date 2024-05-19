7b84426a3771c923bf4c23f0f499a55e76b1685d
Krauser Huang <crescentdence@gmail.com>
2024-05-19 14:25:08 +0800

    [Patch] Fix Click Me background color to blue(was red)

--- SwiftfulSourceControlDemo/NextViewController.swift
+++ SwiftfulSourceControlDemo/NextViewController.swift
@@ -32,7 +32,7 @@ class NextViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .blue
     }
     
 
